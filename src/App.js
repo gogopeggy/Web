f03934c0db308c83fab5e88730c69d19bf14c15b
gogopeggy <a29098477@gmail.com>
2024-07-28 16:54:12 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 import Navbar from "./Navbar";
 import NotFound from "./NotFound";
-import Datepicker from "./components/datepicker";
+// import Datepicker from "./components/datepicker";
 import Home from "./Home";
 import RecipeList from "./pages/recipeList";
+import Money from "./pages/money";
 
 function App() {
-  const today = new Date();
+  // const today = new Date();
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <div className="content">
-          <Routes>
-            <Route exact path="/" element={<Home />}>
-              {/* <Datepicker anyDate={today} /> */}
-            </Route>
-            <Route path="*" element={<NotFound />}></Route>
-            <Route path="/recipe" element={<RecipeList />}></Route>
-          </Routes>
+    <LocalizationProvider dateAdapter={AdapterMoment}>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <div className="content">
+            <Routes>
+              <Route exact path="/" element={<Home />}>
+                {/* <Datepicker anyDate={today} /> */}
+              </Route>
+              <Route path="*" element={<NotFound />}></Route>
+              <Route path="/recipe" element={<RecipeList />}></Route>
+              <Route path="/money" element={<Money />}></Route>
+            </Routes>
+          </div>
         </div>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </LocalizationProvider>
   );
 }
 
