e6951adcac4ef780ba9770267f00fb9148861129
gogopeggy <a29098477@gmail.com>
2024-08-13 22:07:00 +0800

    add location card and fix map key

--- src/pages/camping/map.js
+++ src/pages/camping/map.js
@@ -1,50 +1,94 @@
-import React, { useEffect, useRef, useState } from "react";
-import {
-  APIProvider,
-  Map,
-  Marker,
-  useMarkerRef,
-} from "@vis.gl/react-google-maps";
+import React, { useEffect, useState } from "react";
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+// import Card from "@mui/material/Card";
+// import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 export default function Mapbox() {
-  const [markerRef, marker] = useMarkerRef();
+  // const [markerRef, marker] = useMarkerRef();
+  const Key = process.env.REACT_APP_API_MAPS;
   const locations = [
-    { lat: 25.0410887583241, lng: 121.5203191419131 },
-    { lat: 25.092942603441546, lng: 121.525899341745761 },
-    { lat: 25.091854377536897, lng: 121.56812803669244 },
-    { lat: 25.05453792265606, lng: 121.59782545224441 },
-    { lat: 25.02887626611717, lng: 121.4529432588325 },
-    { lat: 25.001847196692243, lng: 121.58140718430352 },
-    { lat: 23.003804245607544, lng: 120.16124013732555 },
+    { lat: 25.0410887583241, lng: 121.5203191419131, name: "中正紀年堂" },
+    { lat: 25.092942603441546, lng: 121.525899341745761, name: "士林捷運站" },
+    { lat: 25.091854377536897, lng: 121.56812803669244, name: "金面山步道" },
+    {
+      lat: 25.05453792265606,
+      lng: 121.59782545224441,
+      name: "台北流行音樂中心",
+    },
+    { lat: 25.02887626611717, lng: 121.4529432588325, name: "板橋藝文特區" },
+    {
+      lat: 25.001847196692243,
+      lng: 121.58140718430352,
+      name: "台北市立動物園",
+    },
+    { lat: 23.003804245607544, lng: 120.16124013732555, name: "安平老街" },
   ];
-  // const bounds = {
-  //   north: 25.5, // Slightly increased from 25.3
-  //   south: 21.5, // Slightly decreased from 21.8 to allow more southern panning
-  //   west: 118.5, // Slightly decreased from 119.3 to allow more western panning
-  //   east: 122.5,
-  // };
+
+  const bounds = {
+    north: 28.5,
+    south: 21.5,
+    west: 117.5,
+    east: 124.5,
+  };
+
+  const [hoveredMarker, setHoveredMarker] = useState(null);
+
+  const handleMouseOver = (position) => {
+    setHoveredMarker(position);
+  };
+
+  const handleMouseOut = () => {
+    setHoveredMarker(null);
+  };
+
   useEffect(() => {
-    if (!marker) {
-      return;
-    }
+    // if (!marker) {
+    //   return;
+    // }
     // do something with marker instance here
-  }, [marker]);
+  }, []);
 
   return (
-    <APIProvider apiKey={""}>
+    <APIProvider apiKey={Key}>
       <Map
-        // zoom={12}
+        defaultTilt={20}
         style={{ width: "60vw", height: "70vh" }}
-        center={{ lat: 25.0410887583241, lng: 120.9605 }}
-        // restriction={{
-        //   latLngBounds: bounds,
-        //   strictBounds: true, // Prevent the user from panning outside the bounds
-        // }}
+        defaultCenter={{ lat: 25.0410887583241, lng: 120.9605 }}
+        restriction={{
+          latLngBounds: bounds,
+        }}
       >
         {/* <Marker ref={markerRef} position={{ lat: 53.54992, lng: 10.00678 }} /> */}
         {locations.map((position, index) => (
-          <Marker key={index} position={position} />
+          <>
+            <Marker
+              key={index}
+              position={position}
+              onMouseOver={() => handleMouseOver(position)}
+              onMouseOut={handleMouseOut}
+            />
+          </>
         ))}
+        {hoveredMarker && (
+          <div
+            style={{
+              position: "absolute",
+              bottom: "10px",
+              left: "10px",
+              padding: "10px",
+              backgroundColor: "white",
+              boxShadow: "0px 0px 10px rgba(0,0,0,0.3)",
+              borderRadius: "5px",
+              zIndex: 1000,
+            }}
+          >
+            <Typography fontWeight={"bold"} fontSize={14} pb={2}>
+              地標:
+            </Typography>
+            <Typography fontSize={12}>{hoveredMarker.name}</Typography>
+          </div>
+        )}
       </Map>
     </APIProvider>
   );
