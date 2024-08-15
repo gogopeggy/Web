89f0023f300ecb03dea30aa8e1b68d823b7cc9fe
gogopeggy <a29098477@gmail.com>
2024-08-15 17:01:57 +0800

    adjust profile

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -3,21 +3,37 @@ import page1 from "../../assets/profile/Slide1.PNG";
 import page2 from "../../assets/profile/Slide2.PNG";
 import page3 from "../../assets/profile/Slide3.PNG";
 import page4 from "../../assets/profile/Slide4.PNG";
+import webLogo from "../../assets/webLogo.png";
 
 const Profile = ({ user }) => {
   const pageList = [page1, page2, page3, page4];
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
-      <p>{user && `Hi ${user}, please check my profile below:`}</p>
-      {pageList.map((list, index) => (
-        <img
-          src={list}
-          alt={`page${index}`}
-          width={"100%"}
-          style={{ paddingBottom: 4 }}
-        ></img>
-      ))}
+      {user === undefined ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <img src={webLogo} alt="logo" height={"200vh"}></img>
+        </div>
+      ) : (
+        <>
+          <p>{`Hi ${user}, please check my profile below:`}</p>
+          {pageList.map((list, index) => (
+            <img
+              src={list}
+              alt={`page${index}`}
+              width={"100%"}
+              style={{ paddingBottom: 4 }}
+            ></img>
+          ))}
+        </>
+      )}
     </div>
   );
 };
