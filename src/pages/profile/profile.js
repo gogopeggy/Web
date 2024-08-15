05d375c13abd0b3cdb0e61b1f2a0f45671bc6d28
gogopeggy <a29098477@gmail.com>
2024-08-15 17:42:44 +0800

    add slogan

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -4,6 +4,7 @@ import page2 from "../../assets/profile/Slide2.PNG";
 import page3 from "../../assets/profile/Slide3.PNG";
 import page4 from "../../assets/profile/Slide4.PNG";
 import webLogo from "../../assets/webLogo.png";
+import Stack from "@mui/material/Stack";
 
 const Profile = ({ user }) => {
   const pageList = [page1, page2, page3, page4];
@@ -11,7 +12,8 @@ const Profile = ({ user }) => {
   return (
     <div style={{ height: "100vh", width: "100%" }}>
       {user === undefined ? (
-        <div
+        <Stack
+          direction={"column"}
           style={{
             display: "flex",
             justifyContent: "center",
@@ -20,7 +22,12 @@ const Profile = ({ user }) => {
           }}
         >
           <img src={webLogo} alt="logo" height={"200vh"}></img>
-        </div>
+          <img
+            src="https://readme-typing-svg.demolab.com?font=Fira+Code&weight=500&size=20&pause=1000&color=3b699f&center=true&vCenter=true&width=600&height=60&lines=Welcome to Peggyideas!;Be curious, then create your ideas!"
+            alt="Typing SVG"
+            width={"100%"}
+          ></img>
+        </Stack>
       ) : (
         <>
           <p>{`Hi ${user}, please check my profile below:`}</p>
