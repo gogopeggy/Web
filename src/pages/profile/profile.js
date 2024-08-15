b97947f8278506aa7386f2870fe5658fccd660a8
gogopeggy <a29098477@gmail.com>
2024-08-15 15:00:34 +0800

    Add profile

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,23 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import profileFile from "../../assets/profile/Personal Profile.pdf";
 import liff from "@line/liff";
 
 const Profile = () => {
   const LIFF_ID = process.env.REACT_APP_LIFF_ID;
+  const [user, setUser] = useState();
   useEffect(() => {
+    console.log("LIFF_ID", LIFF_ID);
     liff
-      .init({ liffId: { LIFF_ID } })
+      .init({ liffId: LIFF_ID })
       .then(() => {
         console.log("LIFF initialized");
+        getUser();
       })
       .catch((err) => {
         console.error("LIFF Initialization failed", err);
       });
+    getUser();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const getUser = () => {
+    liff
+      .getProfile()
+      .then((profile) => {
+        const name = profile.displayName;
+        setUser(name);
+      })
+      .catch((err) => {
+        console.log("get profile error", err);
+      });
+  };
+
   return (
     <div style={{ height: "100vh", width: "100%" }}>
+      <p>{user}</p>
       <iframe
         src={profileFile}
         width="100%"
